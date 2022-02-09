983d97c67d4fb1cb5dc6fc5e547992433f6a57f1
Brennan <brennan@bowtie.co>
2022-02-09 08:49:26 -0600

    Set up search & display basic data on single view

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['www.thecocktaildb.com'],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/cocktails/[id].tsx
+++ src/pages/cocktails/[id].tsx
@@ -1,11 +1,98 @@
-import type { NextPage } from 'next';
-import { useRouter } from 'next/router';
+import { CircularProgress } from '@mui/material';
+import type { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { getCocktail, searchCocktails } from '../../api/cocktail';
+import { Cocktail, IngredientObject } from '../../models';
+import { useCallback, useEffect, useState } from 'react';
 
-const CocktailSingle: NextPage = () => {
-  const router = useRouter();
-  const { id } = router.query;
+const CocktailSingle = ({ cocktail }: { cocktail: Cocktail }) => {
+  const { strDrink, strDrinkThumb, strInstructions } = cocktail || {};
+  const [ingredientsList, setIngredientsList] = useState<IngredientObject[]>([]);
 
-  return <h2>Cocktail ID#: {id}</h2>;
+  const buildIngredientsList = useCallback((): void => {
+    const ingredients = [];
+    const entries = Object.entries(cocktail).filter(
+      ([key, value]) => (key.includes('strIngredient') || key.includes('strMeasure')) && !!value,
+    );
+
+    for (let i = 1; i <= 15; i++) {
+      const ingredient = entries.find(([key]) => key === `strIngredient${i}`)?.[1]?.trim();
+      const measure = entries.find(([key]) => key === `strMeasure${i}`)?.[1]?.trim();
+      if (ingredient) {
+        ingredients.push({ ingredient, measure });
+      }
+    }
+    setIngredientsList(ingredients);
+  }, [cocktail]);
+
+  useEffect(() => {
+    if (cocktail) {
+      const ingredients = [];
+
+      const entries = Object.entries(cocktail).filter(
+        ([key, value]) => (key.includes('strIngredient') || key.includes('strMeasure')) && !!value,
+      );
+
+      for (let i = 1; i <= 15; i++) {
+        const ingredient = entries.find(([key]) => key === `strIngredient${i}`)?.[1]?.trim();
+        const measure = entries.find(([key]) => key === `strMeasure${i}`)?.[1]?.trim();
+        if (ingredient) {
+          ingredients.push({ ingredient, measure });
+        }
+      }
+      buildIngredientsList();
+    }
+  }, [cocktail, buildIngredientsList]);
+
+  return (
+    <>
+      {cocktail ? (
+        <>
+          <Head>
+            <title>Thirsty - {strDrink}</title>
+          </Head>
+
+          <Image className="round-image" src={strDrinkThumb} alt={strDrink} width={200} height={200} />
+
+          <h1>{strDrink}</h1>
+
+          <h2>Ingredients:</h2>
+          <ul>
+            {ingredientsList.map((item, index) => (
+              <li key={`ingredient-list-${index}`}>
+                {item.ingredient} {item.measure && `(${item.measure})`}
+              </li>
+            ))}
+          </ul>
+
+          <p>{strInstructions}</p>
+        </>
+      ) : (
+        <CircularProgress />
+      )}
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const cocktail = await getCocktail(`${params?.id}`);
+
+  return {
+    props: { cocktail },
+  };
+};
+
+// TODO: Is this that helpful?
+export const getStaticPaths = async () => {
+  const cocktails = await searchCocktails('a');
+  const paths = cocktails.map(cocktail => ({ params: { id: cocktail.idDrink } }));
+  console.log('getStaticPaths', paths);
+
+  return {
+    paths,
+    fallback: true,
+  };
 };
 
 export default CocktailSingle;
